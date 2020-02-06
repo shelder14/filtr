4e3a65518dd96ae7cc14316f37dd11e5d14dd28b
shelder14 <shelder144@yandex.ru>
2020-02-06 16:49:52 +0300

    last55

--- js/script.js
+++ js/script.js
@@ -6,17 +6,16 @@ var app = new Vue({
   },
 mounted() {
   	fetch("https://modx-sample.se7.ru/api/products/getList")
-            .then(function(response){
-				var mas = ['iPhone 7', 'Galaxy S8', 'Nokia N8', 'Xiaomi Mi6']
-            	results = mas;
-            	console.log(this.results);
-            	return mas;
-                   
+            .then((response) => {
+                if(response.ok) {
+                    return response.json();
+                } 
             })
-    //         .then(function(json) {
-				// this.products = json;
-				// console.log(this.products["results"][0]["id"]);			 
-    //         })
+            .then((json) => {
+
+  				    console.log(json["results"]);
+              this.results = json["results"];		 
+            })
             .catch((error) => {
                 console.log('parsing failed');
             });
